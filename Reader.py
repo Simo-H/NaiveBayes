730374ad9440bae43986632b7147dd32026f2af8
Simo <a>
2017-06-05 15:28:12 +0300

    start

--- Reader.py
+++ Reader.py
@@ -1,12 +1,23 @@
+from _ast import In
+
 import pandas as pd
 attributeDictionary = {}
 with open('C:\\Users\\Simo\\Desktop\\NaiveBayesData\\Structure.txt','r') as dataStructureFile:
     for line in dataStructureFile:
             splitted = line.split();
             attributeDictionary[splitted[1]] = splitted[2]
-data = pd.read_csv("C:\\Users\\Simo\\Desktop\\NaiveBayesData\\test.csv");
+data = pd.read_csv("C:\\Users\\Simo\\Desktop\\NaiveBayesData\\train.csv");
 # groupByclassData = data.groupby('class');
 # data.groupby(['class'])['age'].mean()
-ageAvg = data.groupby(['class'],as_index=True)['age'].mean()
-
-print ()
+print(data.isnull().sum())
+print("\n --------------------------------- \n")
+for key in attributeDictionary:
+    if(attributeDictionary[key] == 'NUMERIC'):
+        data[key] = data.groupby("class").transform(lambda x: x.fillna(x.mean()))
+    else:
+        max_item = data[key].value_counts().idxmax()
+        # max_item = items_counts.idmax()
+        data[key] = data.groupby("class").transform(lambda x: x.fillna(max_item))
+print(data.isnull().sum())
+# noClassAvg = Avg[0];
+# yesClassAvg = Avg[1];
